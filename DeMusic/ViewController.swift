3737af0927249c016dd8969ec26eb4df5e89ed4b
preatorianwise999 <preatorianwise999@gmail.com>
2019-11-06 20:37:20 -0300

    first commit

---
+++ DeMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeMusic
+//
+//  Created by Jose David Bustos H on 11/6/19.
+//  Copyright © 2019 Jose David Bustos H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
