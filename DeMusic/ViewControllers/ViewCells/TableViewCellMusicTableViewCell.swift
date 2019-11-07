1fd994a7a863eab6957c7b0cbc064b3d060f28c6
preatorianwise999 <preatorianwise999@hotmail.com>
2019-11-07 20:23:38 -0300

    salvando tableviewcells

---
+++ DeMusic/ViewControllers/ViewCells/TableViewCellMusicTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCellMusicTableViewCell.swift
+//  DeMusic
+//
+//  Created by Jose David Bustos H on 11/7/19.
+//  Copyright © 2019 Jose David Bustos H. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellMusicTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
