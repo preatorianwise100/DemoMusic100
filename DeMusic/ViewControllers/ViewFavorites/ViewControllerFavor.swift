009a76e99bfd307a4755d83b70b34be48d877e9c
preatorianwise999 <preatorianwise999@hotmail.com>
2019-11-07 20:26:35 -0300

    salvando add new viewcontrollers

---
+++ DeMusic/ViewControllers/ViewFavorites/ViewControllerFavor.swift
@@ -0,0 +1,25 @@
+//
+//  ViewControllerFavor.swift
+//  DeMusic
+//
+//  Created by Jose David Bustos H on 11/7/19.
+//  Copyright © 2019 Jose David Bustos H. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerFavor: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
