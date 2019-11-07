cd24bd1939a6c71326da3b2faee3c90157e050e5
preatorianwise999 <preatorianwise999@hotmail.com>
2019-11-07 00:19:23 -0300

    salvando items 2

---
+++ DeMusic/ItemMusicModel.swift
@@ -0,0 +1,9 @@
+//
+//  ItemMusicModel.swift
+//  DeMusic
+//
+//  Created by Jose David Bustos H on 11/6/19.
+//  Copyright © 2019 Jose David Bustos H. All rights reserved.
+//
+
+import Foundation
---
+++ DeMusic/ItemMusicProvider.swift
@@ -0,0 +1,9 @@
+//
+//  ItemMusicProvider.swift
+//  DeMusic
+//
+//  Created by Jose David Bustos H on 11/7/19.
+//  Copyright © 2019 Jose David Bustos H. All rights reserved.
+//
+
+import Foundation
